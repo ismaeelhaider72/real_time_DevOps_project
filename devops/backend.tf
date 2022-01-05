5f21f11f0d1412fd88314aed88fa83fbefff6056
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2022-01-05 16:41:19 +0500

    Update backend.tf

--- devops/backend.tf
+++ devops/backend.tf
@@ -3,7 +3,6 @@ terraform {
  backend "s3" {
    bucket = "ismaeelhtmltemplatesbucket"
    key    = "jmsth_jenkins.tfstate"
-   region = "us-east-2"
    encrypt = true
    dynamodb_table = "terraform-state-lock-dynamo"
    }
